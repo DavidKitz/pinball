05d52630df43708eaf71f5beffe1f69ea093fb23
DavidKitz <david.kitzwork@gmail.com>
2022-03-11 13:09:26 +0100

    Initial commit

---
+++ src/test/java/com/example/pinball/PinballApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pinball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinballApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
